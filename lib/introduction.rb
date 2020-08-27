4425d89c0f9365fffaefa4cbdd503d588cbc6c19
Brian Sahota <bpsahota@gmail.com>
2020-08-27 21:13:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,13 +7,13 @@ introduction("Josh")
 
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language("Brian", "Ruby")
+introduction_with_language("Dan", "Ember")
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language_optional("Brian")
